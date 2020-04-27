6af262f90ec47228f0be28305fa4817733fb8f39
liusheng <liusheng2048@gmail.com>
2020-04-27 09:41:31 +0800

    Test Travis ARM CI

--- dev-tools/travis/travis-install.sh
+++ dev-tools/travis/travis-install.sh
@@ -13,7 +13,8 @@
 
 echo "Python version :  " `python -V 2>&1`
 echo "Python3 version :  " `python3 -V 2>&1`
-echo "Pip version :  " `pip --version 2>&1`
+# in ARM travis env, the "pip" binary refer to pip3 than pip2, so need to explicitly specify pip3
+echo "Pip2 version :  " `pip2 --version 2>&1`
 echo "Pip3 version :  " `pip3 --version 2>&1`
 
 
@@ -23,7 +24,7 @@ STORM_SRC_ROOT_DIR=$1
 
 TRAVIS_SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-pip install --user -r ${TRAVIS_SCRIPT_DIR}/requirements.txt
+pip2 install --user -r ${TRAVIS_SCRIPT_DIR}/requirements.txt
 pip3 install --user -r ${TRAVIS_SCRIPT_DIR}/requirements.txt
 
 python ${TRAVIS_SCRIPT_DIR}/save-logs.py "storm-shaded-deps/install-shade.txt" mvn clean install --batch-mode -pl storm-shaded-deps -am
